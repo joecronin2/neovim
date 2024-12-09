39d8c3cc0abd5380ad02515419105b92d49015e7
Joe Cronin <joseph.cronin10@gmail.com>
2024-12-09 20:58:23 +0100

    updated mappings

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -5,11 +5,12 @@ map({ "n", "v" }, "h", "j")
 map({ "n", "v" }, "u", "k")
 
 -- scroll UH
-map({ "n", "v" }, "U", "2<C-y>2k")
-map({ "n", "v" }, "H", "2<C-e>2j")
+map({ "n", "v" }, "U", "3<C-y>3k")
+map({ "n", "v" }, "H", "3<C-e>3j")
 
--- 'L
-map({ "n", "v" }, "'", "h")
+-- ;L
+map({ "n", "v" }, ";", "h")
+map({ "n", "v" }, "l", "l")
 
 -- new line
 map({ "n", "v", "i" }, "<C-h>", "<esc>o")
@@ -41,17 +42,20 @@ map({ "n", "v" }, "m", "n")
 map({ "n", "v" }, "M", "N")
 
 -- Buffer
-map({'n','v','i'}, "<leader>e","<cmd>BufferLineCyclePrev<cr>")
-map({'n','v','i'}, "<leader>t","<cmd>BufferLineCycleNext<cr>")
-map({'n','v','i'}, "<C-x>","<cmd>BufferLinePickClose<cr>")
+map({ "n", "v" }, "<leader>e", "<cmd>BufferLineCyclePrev<cr>")
+map({ "n", "v" }, "<leader>t", "<cmd>BufferLineCycleNext<cr>")
+map({ "n", "v", "i" }, "<C-x>", "<cmd>BufferLinePickClose<cr>")
 
 -- Jump
-map({'n','v'}, "<leader>d", "<cmd>Telescope lsp_definitions<cr>")
-map({'n','v'}, "<leader>D", "<cmd>Telescope lsp_incoming_calls<cr>")
-map({'n','v'}, "<leader>g", "<cmd>Telescope live_grep<cr>")
-map({'n','v'}, "<leader>rs", "<cmd>Gitsigns stage_hunk<cr>")
-map({'n','v'}, "<leader>rS", "<cmd>Gitsigns reset_hunk<cr>")
-map({'n','v'}, "<leader>n", "<cmd>Navbuddy<cr>")
+map({ "n", "v" }, "<leader>d", "<cmd>Telescope lsp_definitions<cr>")
+map({ "n", "v" }, "<leader>D", "<cmd>Telescope lsp_incoming_calls<cr>")
+map({ "n", "v" }, "<leader>g", "<cmd>Telescope live_grep<cr>")
+map({ "n", "v" }, "<leader>rs", "<cmd>Gitsigns stage_hunk<cr>")
+map({ "n", "v" }, "<leader>rS", "<cmd>Gitsigns reset_hunk<cr>")
+map({ "n", "v" }, "<leader>n", "<cmd>Navbuddy<cr>")
+
+-- terminal
+map({ "n", "v" }, "<leader>T", "<cmd>Lspsaga term_toggle<cr>")
 
 -- Format
 map({ "n", "v" }, "<leader>l", function()
@@ -64,10 +68,13 @@ map({ "n", "v" }, "<leader>q", function()
 end)
 
 -- Don't yank selection on delete
-map("x", "<leader>p", "\"_dP")
+map("x", "<leader>p", '"_dP')
 
 -- delete word
-map({'n','v','i'}, "<C-c>", "ciw")
+map({ "n", "v", "i" }, "<C-c>", "<esc>ciw")
 
 -- cool stuff
 map("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
+map("n", "<leader>fmg", "<cmd>CellularAutomaton game_of_life<CR>")
+map("n", "<leader>fms", "<cmd>CellularAutomaton scramble<CR>")
+map("n", "<leader>fmt", "<cmd>Tetris<CR>")
